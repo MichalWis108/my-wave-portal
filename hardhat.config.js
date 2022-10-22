de3498b86363f779dd4fd7eb1c5284f27614a7f9
MichalWis108 <m.wisniewski108@gmail.com>
2022-10-22 13:14:56 +0100

    2nd commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
