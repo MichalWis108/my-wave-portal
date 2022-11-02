d28540db832b7c352acd958fb460a50bb5753dc5
MichalWis108 <m.wisniewski108@gmail.com>
2022-11-02 06:36:45 +0000

    commit

--- scripts/run.js
+++ scripts/run.js
@@ -2,9 +2,19 @@
 const main = async () => {
   const [owner, randomPerson, randomPersonTwo] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await waveContract.deployed();
   console.log("Contract addy:", waveContract.address);
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
@@ -18,6 +28,12 @@ const main = async () => {
 
   waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
   await waveTxn.wait(); // Wait for the transaction to be mined
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
